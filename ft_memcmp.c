a46806cd007b6ffc2d82d5aa0bd4f2be99b0317d
Roger HOBBS <rhobbs@c5r10s6.wethinkcode.co.za>
2019-06-06 13:15:50 +0200

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rhobbs <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 10:56:25 by rhobbs            #+#    #+#             */
-/*   Updated: 2019/06/01 15:31:34 by rhobbs           ###   ########.fr       */
+/*   Updated: 2019/06/06 13:12:41 by rhobbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str2 = (unsigned char*)s1;
 	str1 = (unsigned char*)s2;
-	while (i < (n - 1) && str1[i] == str2[i])
-		i++;
-	return (str2[i] - str1[i]);
+	if (n == 0)
+		return (0);
+	else
+	{
+		while (i < (n - 1) && str1[i] == str2[i])
+			i++;
+		return (str2[i] - str1[i]);
+	}
 }
