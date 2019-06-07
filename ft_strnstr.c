9e21a767132db861ed0bb34bcaf6e6bb53d72a88
Roger HOBBS <rhobbs@c5r10s6.wethinkcode.co.za>
2019-06-07 14:36:30 +0200

    shorter prototype strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,29 +6,28 @@
 /*   By: rhobbs <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 06:54:50 by rhobbs            #+#    #+#             */
-/*   Updated: 2019/06/01 16:26:36 by rhobbs           ###   ########.fr       */
+/*   Updated: 2019/06/07 14:34:38 by rhobbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *hay, const char *ndl, size_t len)
 {
 	int			check;
 	size_t		nlen;
 	int			i;
 
 	i = 0;
-	nlen = ft_strlen(needle);
-	if (!*needle)
-		return ((char *)haystack);
-	while ((haystack[i]) && nlen <= (len - i))
+	nlen = ft_strlen(ndl);
+	if (!*ndl)
+		return ((char *)hay);
+	while ((hay[i]) && nlen <= (len - i))
 	{
 		check = 1;
-		check = ft_strncmp(&haystack[i], needle, nlen);
+		check = ft_strncmp(&hay[i], ndl, nlen);
 		if (check == 0)
-			return ((char *)&haystack[i]);
+			return ((char *)&hay[i]);
 		i++;
 	}
 	return (NULL);
