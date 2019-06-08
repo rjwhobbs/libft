11aeaef575edb0ebb51733f59e061fa936133b73
Roger HOBBS <rhobbs@c5r10s6.wethinkcode.co.za>
2019-06-08 16:16:42 +0200

    Temp YES

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rhobbs <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:31:24 by rhobbs            #+#    #+#             */
-/*   Updated: 2019/06/08 13:19:38 by rhobbs           ###   ########.fr       */
+/*   Updated: 2019/06/08 14:51:10 by rhobbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,5 +78,6 @@ void				ft_memdel(void **ap);
 char				**ft_strsplit(char const *s, char c);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
 
 #endif
